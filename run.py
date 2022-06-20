e2342d15b921f26d7824b7ae772d9cb51346b58f
Miles-Cownie <miles_cownie94@protonmail.com>
2022-06-20 10:07:12 +0000

    Add name input function

--- run.py
+++ run.py
@@ -7,3 +7,11 @@ It also imports the word lists from the other two python files
 import random
 from words_easy import easy_word_list
 from words_hard import hard_word_list
+
+
+def choose_name():
+    name = input("Please enter your name\n")
+    print(f"Hello, {name.capitalize()}, Let's get started!")
+
+
+choose_name()
